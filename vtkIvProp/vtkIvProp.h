2ecb79f0e1d866db54a69b932d0f8f7fb8b5e810
unknown <i.wolf@dkfz.de>
2010-05-18 14:41:38 -0400

    initial commit with an empty skeleton of the vtkIvProp

---
+++ vtkIvProp/vtkIvProp.h
@@ -0,0 +1,56 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date: 2006-05-04 16:45:18 +0200 (Do, 04 Mai 2006) $
+Version:   $Revision: 6790 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef vtkIvProp_H_HEADER_INCLUDED_C1C53723
+#define vtkIvProp_H_HEADER_INCLUDED_C1C53723
+
+#include "vtkProp.h"
+
+class /*VTK_IV_EXPORT*/ vtkIvProp : public vtkProp
+{
+  public:
+    static vtkIvProp* New();
+    vtkTypeMacro(vtkIvProp,vtkProp);
+
+    int RenderOpaqueGeometry(vtkViewport* viewport);
+    
+    int RenderOverlay(vtkViewport* viewport);
+
+    double *GetBounds();
+
+    void ReleaseGraphicsResources(vtkWindow* window);
+
+    void InitPathTraversal();
+
+    vtkAssemblyPath* GetNextPath();
+ 
+    int HasTranslucentPolygonalGeometry();
+    int RenderTranslucentPolygonalGeometry( vtkViewport *);
+    int RenderVolumetricGeometry( vtkViewport *);    
+
+  protected:
+    
+    vtkIvProp();
+    virtual ~vtkIvProp();
+
+};
+
+
+#endif /* vtkIvProp_H_HEADER_INCLUDED_C1C53723 */
+
+
